eb3a89ebda1d6c31aad324906d81eb4f820a8070
yrboyogueno <yr_boyogueno@csf.bc.ca>
2026-05-10 03:35:07 -0700

    Create appLocalStorage.js

---
+++ JS/appLocalStorage.js
@@ -0,0 +1,147 @@
+
+// Récupération des boutons
+const BtnLogin = document.getElementById("btnLogin");
+const BtnRegister = document.getElementById("btnRegister");
+
+//  Récupération des conteneurs de formulaires
+const LoginContainer = document.getElementById("loginFormContainer");
+const RegisterContainer = document.getElementById("registerFormContainer");
+
+
+//  Récupération des données
+const LoginForm = document.getElementById("loginForm");
+const RegisterForm = document.getElementById("registerForm");
+
+//  Récupération des erreurs
+const LoginError = document.getElementById("loginError");
+const RegisterError = document.getElementById("registerError")
+
+
+//Champs formulaire de connexion
+const LoginEmail = document.getElementById("loginEmail");
+const LoginPassword = document.getElementById("loginPassword");
+
+
+
+//Champs du formulaire d'inscription
+const RegisterEmail = document.getElementById("registerEmail");
+const RegisterPassword = document.getElementById("registerPassword");
+
+
+//  Quand on clique sur "Se connecter", on fait apparître le formulaire
+BtnLogin.addEventListener("click", () => {
+    LoginContainer.classList.remove("hidden");
+    RegisterContainer.classList.add("hidden");
+    LoginError.textContent = "";
+});
+
+
+//  Quand on clique sur "S'inscrire", on fait apparaître le formulaire
+BtnRegister.addEventListener("click", () => {
+    RegisterContainer.classList.remove("hidden");
+    LoginContainer.classList.add("hidden");
+    LoginError.textContent = "";
+});
+
+
+
+//  Fonction de validation de l'email
+function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email.trim());
+}
+
+
+// Fonction de validation du mot de passe
+function validatePassword (password) {
+    const miniLength = /.{6,}/;
+    const lowercase = /[a-z]/;
+    const uppercase = /[A-Z]/;
+    const number = /[0-9]/;
+    const specialChar = /[!@#$%?&*(),.?":{}|<>]/;
+
+
+    return (
+        miniLength.test(password) &&
+        lowercase.test(password) &&
+        uppercase.test(password) &&
+        number.test(password) &&
+        specialChar.test(password)
+    );
+}
+
+
+
+//Contrôle des données du formulaire d'inscription
+RegisterForm.addEventListener("submit", (event) => {
+    event.preventDefault();     //Empêche l'envoi automatique du formulaire
+
+    //  Récupération des valeurs des formulaires
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
+    
+    //  Vérification de l'email
+    if (!validateEmail(email)) {
+        RegisterError.textContent = "Adresse email invalide.";
+        return;
+    }
+
+    //Vérification mot de passe
+    if (!validatePassword(password)) {
+        RegisterError.textContent = "Le mot de passe doit contenir au moins 6 caractères, une minuscule, une majuscule, un chiffre et un caractère spécial.";
+        return;
+    }
+    localStorage.setItem("userEmail", RegisterEmail.value);
+    localStorage.setItem("userPassword", RegisterPassword.value);
+
+
+    LoginError.textContent ="";
+    
+    LoginContainer.classList.remove("hidden");
+    RegisterContainer.classList.add("hidden");
+    LoginError.textContent = "";
+}
+
+)
+
+
+
+
+//  Contrôle des données du formulaire de connexion
+LoginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //  Récupération des valeurs des formulaires
+    const email = LoginEmail.value;
+    const password = LoginPassword.value;
+
+        //  Vérification de l'email
+    if (!validateEmail(email)) {
+        LoginError.textContent = "Adresse email invalide.";
+        return;
+    }
+
+    //Vérification mot de passe
+    if (!validatePassword(password)) {
+        LoginError.textContent = "Le mot de passe doit contenir au moins 6 caractères, une minuscule, une majuscule, un chiffre et un caractère spécial.";
+        return;
+    }
+
+    const valideEmail = localStorage.getItem("userEmail");
+    const validePassword = localStorage.getItem("userPassword");
+
+
+    if (email == valideEmail && password == validePassword) {
+        LoginError.textContent = "";
+        window.location.href = "HTML/profil.html";
+    }else {
+        LoginError.textContent = "Identifiants incorrects" ;
+    }
+
+}
+)
+
+
+
+
+
+
